a013c97155a1fb099165e28aab8a8882a79613a5
Saqib sattar <saqibkhanz1234@gmail.com>
2019-10-09 23:09:50 +0500

    BroadCasting Messages

--- index.js
+++ index.js
@@ -20,4 +20,8 @@ socket.on('chat', function(data){
     io.sockets.emit('chat', data);
 });
 
+socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+})
+
 });
--- public/chat.js
+++ public/chat.js
@@ -7,6 +7,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 // Emit Event
 
@@ -17,7 +18,18 @@ btn.addEventListener('click', function(){
     })
 });
 
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
+
+
 // Listen for event
 socket.on('chat', function(data) {
+    feedback.innerHTML = "";
     output.innerHTML += "<p><strong> " + data.handle + ": </strong>" + data.message + "</p>";
-})
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = "<p><em>" + data + " is typing....</em></p>";
+});
+
